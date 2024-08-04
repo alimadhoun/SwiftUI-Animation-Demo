cd07bee60a9f7f6fede01c0b5041196d809c8129
Ali~ <30940768+alimadhoun@users.noreply.github.com>
2024-08-04 14:05:29 +0300

    Initial Commit

---
+++ animationDemo/animationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  animationDemoApp.swift
+//  animationDemo
+//
+//  Created by ali madhoun on 04/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct animationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
